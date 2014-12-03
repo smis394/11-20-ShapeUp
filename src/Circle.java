a5a9e68e539bfa10062461f266f285f0ff1f2d54
smis394 <shambhavimishra46@gmail.com>
2014-12-02 19:11:30 -0700

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,1 +1,60 @@
 // A Circle object represents a circle defined by a center point and a radius.
+public class Circle 
+{
+  Point center;
+  double radius;
+  
+  public Circle() 
+  {
+    center = new Point();
+    radius = 0;
+  }
+  
+  public Circle(Point startPoint, Point endPoint)
+  {
+    center = startPoint;
+    radius = center.distance(endPoint);
+  }
+  
+  public Circle(Point m, double n) 
+  {
+    center = m;
+    radius = n;
+  }
+  
+  public double getRadius() 
+  {
+    return radius;
+  }
+  
+  public double area()
+  {
+    return (double)(3.14 * (radius * radius));
+  }
+  
+  public double circumfrence() 
+  {
+  return (double)(2 * radius * 3.14);
+  }
+  
+  public double diameter()
+  {
+    return (double)(2 * radius);
+  }
+  
+  public String toString() 
+  {
+    return "[" + center.toString() + ", " + radius + "]";
+  }
+  
+  public void translate(int x, int y)
+  {
+    center.x += x;
+    center.y += y;
+  }
+  
+  public void changeRadius(double x)
+  {
+    radius += x;
+  }
+}
